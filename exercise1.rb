ee207e9be68a05c0be50f3eb76292f99ed768334
Zebraholic <rctavares@rogers.com>
2014-10-23 14:44:03 -0400

    Added parent Person class.

--- exercise1.rb
+++ exercise1.rb
@@ -1,16 +1,17 @@
 class Person
+	attr_accessor :name
 end
 
 
 
-class Student
+class Student < Person
 	def learn
 		puts "I get it!"
 	end
 end
 
 
-class Instructor 
+class Instructor < Person
 	def teach
 		puts "Everything in Ruby is an Object."
 	end
